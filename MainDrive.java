cf3bd34c294ecb0e0c3ca758753a98a59b073f48
enchantrfan <547686971@qq.com>
2018-11-19 23:39:55 +0800

    疯狂泳池注水员

---
+++ MainDrive.java
@@ -0,0 +1,23 @@
+package homework_08;
+
+import java.util.Calendar;
+import java.util.Timer;
+/**
+ * Created by captain on 2017/7/27.
+ */
+public class MainDrive {
+
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DATE),
+                20,0,0
+        );
+        WaterTimeTask task = new WaterTimeTask();
+        Timer timer = new Timer();
+        timer.schedule(task,1000);
+//        timer.schedule(task,calendar.getTime(),1000*60*60*24);
+    }
+}
---
+++ OutputThread.java
@@ -0,0 +1,34 @@
+package homework_08;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 放水线程
+ * Created by captain on 2017/7/27.
+ */
+public class OutputThread implements Callable<Object>{
+
+    private BlockingQueue<String> queue;
+
+    public OutputThread(BlockingQueue queue){
+        this.queue = queue;
+    }
+
+    @Override
+    public Object call() throws InterruptedException {
+//    	System.out.println(" output");
+        while(true){
+            if(queue.size()==10000){
+                break;
+            }
+            for(int cnt=0;cnt<300;cnt++){
+                queue.poll(10,TimeUnit.SECONDS);
+            }
+            System.out.println("放水3立方米，当前水量："+queue.size());
+            TimeUnit.SECONDS.sleep(1);
+        }
+        return "success";
+    }
+}
